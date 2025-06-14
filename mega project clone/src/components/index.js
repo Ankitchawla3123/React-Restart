ac35119a797026e6cb812e9b1ceba36084aead98
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-06-15 00:38:15 +0530

    protected route learning

--- mega project clone/src/components/index.js
+++ mega project clone/src/components/index.js
@@ -3,12 +3,16 @@ import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import Logoutbtn from "./header/Logoutbtn";
 import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
 
 export{
     Header,
     Footer,
     Container,
     Logoutbtn,
-    Logo
+    Logo,
+    Button,
+    Input
     
 }
